bb3766e86f71dd3998d6e37fb71468c1c9e249ca
Stephen Ashley <stephen_ashley@homedepot.com>
2020-01-16 15:36:44 -0500

    First Commit

---
+++ src/main/java/com/homedepot/ise/sgadb2demo/SgaDb2DemoApplication.java
@@ -0,0 +1,13 @@
+package com.homedepot.ise.sgadb2demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgaDb2DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgaDb2DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/homedepot/ise/sgadb2demo/SgaDb2DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homedepot.ise.sgadb2demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgaDb2DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
